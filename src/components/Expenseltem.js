0e3175fd55c84b44a78546ae57170ddeb3c9e101
Emre YILMAZ <yilmaz0emre@gmail.com>
2021-08-16 20:59:30 +0300

    ExpeseDate dosyası eklendi Date bölümünde görselleştirmelerde iyileştirmeler yapıldı

--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,10 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './Expenseltem.css';
 
 function ExpenseItem(props) 
 {
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString ()}</div>
+      <ExpenseDate date = {props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
