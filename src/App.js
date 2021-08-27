e39bce14d23e00d202ba61cefb524dbffdab9a3c
Emre YILMAZ <yilmaz0emre@gmail.com>
2021-08-27 21:52:22 +0300

    yeni dosyalar eklendi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-//import Todo from './components/Todo';
-import ExpenseItem from "./components/Expenseltem";
+
+import NewEspense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -31,26 +32,8 @@ function App() {
 
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <NewEspense />
+      <Expenses items={expenses}/>
     </div>
   );
 }
